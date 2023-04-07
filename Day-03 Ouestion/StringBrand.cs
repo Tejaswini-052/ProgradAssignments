9e313dfd9c5535faf358034d8aea5f80987896f5
Tejaswini M <86282374+Tejaswini-052@users.noreply.github.com>
2023-04-07 17:33:55 +0530

    Add files via upload

---
+++ Day-03 Ouestion/StringBrand.cs
@@ -0,0 +1,20 @@
+﻿namespace Day_03_Ouestion
+{
+    internal class StringBrand
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a String:");
+            string stringbrand = Console.ReadLine();
+
+            if (stringbrand.Length > 1 && stringbrand[0] == stringbrand[stringbrand.Length - 1])
+            {
+                Console.WriteLine(stringbrand.Substring(0, 1).ToUpper() + stringbrand.Substring(1) + stringbrand.ToLower());
+            }
+            else
+            {
+                Console.WriteLine("The " + stringbrand.Substring(0, 1).ToUpper() + stringbrand.Substring(1).ToLower());
+            }
+        }
+    }
+}
