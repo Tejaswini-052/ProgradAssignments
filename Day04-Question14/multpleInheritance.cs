87c4dfb59a436b5f059f7638e63bb55131d14b69
Tejaswini M <86282374+Tejaswini-052@users.noreply.github.com>
2023-04-07 18:40:35 +0530

    Add files via upload

---
+++ Day04-Question14/multpleInheritance.cs
@@ -0,0 +1,42 @@
+﻿namespace Day04_Question14
+{
+    internal class multpleInheritance
+    {
+        interface IWalk
+        {
+            void Walk();
+        }
+
+        interface ISwim
+        {
+            void Swim();
+        }
+
+        class Animal : IWalk, ISwim
+        {
+            public Animal(string name)
+            {
+                Console.WriteLine($"Animal {name} has been created");
+            }
+
+            public void Walk()
+            {
+                Console.WriteLine("Animal is walking");
+            }
+
+            public void Swim()
+            {
+                Console.WriteLine("Animal is swimming");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal("Nemo");
+            animal.Walk();
+            animal.Swim();
+        }
+
+    }
+
+ }
