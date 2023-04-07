42a34cbe59462d5e7620d684619d47d1475773dc
Tejaswini M <86282374+Tejaswini-052@users.noreply.github.com>
2023-04-07 18:08:43 +0530

    Add files via upload

---
+++ Day03-Ouestion9/CardHash.cs
@@ -0,0 +1,29 @@
+﻿namespace Day03_Ouestion9
+{
+    internal class CardHash
+    {
+        static void Main(string[] args)
+        {
+                Console.WriteLine("Enter the card number:");
+                string cardno = Console.ReadLine();
+                if (cardno.Length <= 4)
+                {
+                   Console.WriteLine(cardno); 
+        
+                }
+                else
+                {
+                    string hashed = "";
+                    for (int i = 0; i < cardno.Length - 4; i++)
+                    {
+                        hashed += "#";
+                    }
+                    hashed += cardno.Substring(cardno.Length - 4);
+                    Console.WriteLine(hashed);
+                }
+
+           
+
+        }
+    }
+}
