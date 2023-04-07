487e40885784429b6fb8e77101c4a345f6ba8f2d
Tejaswini M <86282374+Tejaswini-052@users.noreply.github.com>
2023-04-07 18:41:41 +0530

    Add files via upload

---
+++ Day04-Question15/Multilevel.cs
@@ -0,0 +1,41 @@
+﻿namespace Day04_Question15
+{
+    internal class Block
+    {
+
+        public class Shape
+        {
+            public void Display()
+            {
+                Console.WriteLine("There are different shapes");
+            }
+        }
+
+        public class Triangle : Shape
+        {
+            public void Draw()
+            {
+                Console.WriteLine("Triangle have three sides");
+            }
+        }
+
+        class Equilateral : Triangle
+        {
+            public void sides()
+            {
+                Console.WriteLine("All sides are equal");
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Equilateral Trial = new Equilateral();
+            Trial.Display();
+            Trial.sides();
+            Trial.Draw();
+
+        }
+
+    }
+}
