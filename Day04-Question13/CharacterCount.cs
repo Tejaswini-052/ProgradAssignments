9f1842fb7ff6eb4daae36ec31d3d20349202254e
Tejaswini M <86282374+Tejaswini-052@users.noreply.github.com>
2023-04-07 18:11:15 +0530

    Add files via upload

---
+++ Day04-Question13/CharacterCount.cs
@@ -0,0 +1,27 @@
+﻿namespace Day04_Question13
+{
+    internal class CharacterCount
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string:");
+            string str = Console.ReadLine();
+            while (str.Length > 0)
+            {
+                Console.Write(str[0] + " : ");
+                int count = 0;
+                for (int j = 0; j < str.Length; j++)
+                {
+                    if (str[0] == str[j])
+                    {
+                        count++;
+                    }
+                }
+                Console.WriteLine(count);
+                str = str.Replace(str[0].ToString(), string.Empty);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
