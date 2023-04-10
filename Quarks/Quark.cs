f2783a6a6e5a9d68089c65bed81d4542cd88839a
Tejaswini M <86282374+Tejaswini-052@users.noreply.github.com>
2023-04-10 18:59:06 +0530

    Add files via upload

---
+++ Quarks/Quark.cs
@@ -0,0 +1,42 @@
+﻿namespace Quarks
+{
+    public class Quark
+    {
+        public string Color;
+        public string Flavor;
+        public double BaryonNumber;
+
+        public Quark(string color, string flavor)
+        {
+            this.Color = color;
+            this.Flavor = flavor;
+            this.BaryonNumber = 1.0/ 3.0;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(Color +" "+  Flavor +" "+BaryonNumber);
+        }
+        public void Interact(Quark OtherQuark)
+        {
+            string temp = this.Color;
+            this.Color = OtherQuark.Color;
+            OtherQuark.Color = temp;
+        }
+         
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Before calling interact method:");
+            Quark quarkObject1 = new Quark("Yellow", "up");
+            quarkObject1.Display();
+
+            Quark quarkObject2 = new Quark("Orange", "strange");
+            quarkObject2.Display();
+
+            Console.WriteLine("After calling interact method:");
+            quarkObject1.Interact(quarkObject2);
+            quarkObject1.Display();
+            quarkObject2.Display();
+        }
+    }
+}
