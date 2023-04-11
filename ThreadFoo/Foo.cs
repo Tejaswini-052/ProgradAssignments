33b4c377332ebc9af897b575bb84fc81d2d43316
Tejaswini M <86282374+Tejaswini-052@users.noreply.github.com>
2023-04-11 17:57:50 +0530

    Add files via upload

---
+++ ThreadFoo/Foo.cs
@@ -0,0 +1,144 @@
+﻿namespace ThreadFoo
+{
+    public class Foo
+    {
+
+        public static void first()
+        {
+
+            Console.WriteLine("first");
+
+        }
+
+        public static void second()
+        {
+
+
+            Console.WriteLine("second");
+
+        }
+
+        public static void third()
+        {
+
+            Console.WriteLine("third");
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            int count = 1;
+            int[] n = new int[3];
+            Console.WriteLine("Enter the Order for three methods:");
+            for (int i = 0; i < 3; i++)
+            {
+                n[i] = int.Parse(Console.ReadLine());
+            }
+            Thread ObjOne = new Thread(first);
+            Thread ObjTwo = new Thread(second);
+            Thread ObjThree = new Thread(third);
+            
+
+            while (n[0] == 1 && n[1] == 2)
+            {
+               
+                ObjOne.Start();
+                count = 2;
+                if (count == 2)
+                {
+                    ObjTwo.Start();
+                    count = 3;
+                }
+                if (count == 3)
+                {
+                    ObjThree.Start();
+                }
+                n[0] = 4;
+            }
+
+            while (n[0] == 2 && n[1] == 3)
+            {
+               
+                ObjTwo.Start();
+                count = 2;
+                if (count == 2)
+                {
+                    ObjThree.Start();
+                    count = 3;
+                }
+                if (count ==3)
+                {
+                    ObjOne.Start();
+                }
+                n[0] = 4;
+            }
+
+            while (n[0] == 3 && n[1] == 1)
+            {
+               
+                ObjThree.Start();
+                count = 2;
+                if (count == 2)
+                {
+                    ObjOne.Start();
+                    count = 3;
+                }
+                if  (count ==3 )
+                {
+                    ObjTwo.Start();
+                }
+                n[0] = 4;
+            }
+
+            while (n[0] == 3 && n[1] == 2)
+            {
+               
+                ObjThree.Start();
+                count = 2;
+                if (count == 2)
+                {
+                    ObjTwo.Start();
+                    count = 3;
+                }
+                if (count == 3)
+                {
+                    ObjOne.Start();
+                }
+                n[0] = 4;
+            }
+
+            while (n[0] == 2 && n[1] == 1)
+            {
+                
+                ObjTwo.Start();
+                count = 2;
+                if (count == 2)
+                {
+                    ObjOne.Start();
+                    count = 3;
+                }
+                if (count == 3)
+                {
+                    ObjThree.Start();
+                }
+                n[0] = 4;
+            }
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+    }
+}
