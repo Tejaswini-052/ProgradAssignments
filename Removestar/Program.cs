743e3f84606098ffe14cbdea7d544a95800c3767
Tejaswini M <86282374+Tejaswini-052@users.noreply.github.com>
2023-04-14 18:12:53 +0530

    Add files via upload

---
+++ Removestar/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace RemoveStar
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string");
+            string StringStarRemove = Console.ReadLine();
+            int n = StringStarRemove.Length;
+            Stack<char> stack = new Stack<char>();
+
+            
+            for (int i = 0; i < n; i++)
+            {
+                if (StringStarRemove[i] == '*')
+                {
+                    
+                    while (stack.Count > 0 && stack.Peek() == '*')
+                    {
+                        stack.Pop();
+                    }
+                    if (stack.Count > 0)
+                    {
+                        stack.Pop();
+                    }
+                }
+                else
+                {
+                    stack.Push(StringStarRemove[i]);
+                }
+            }
+
+            
+            char[] arr = stack.ToArray();
+            Array.Reverse(arr);
+
+            
+            Console.WriteLine(arr);
+        }
+    }
+}
