3841cccca7ac8bb656267a80e765fc4f3a2f511e
Tejaswini M <86282374+Tejaswini-052@users.noreply.github.com>
2023-04-14 18:51:55 +0530

    Add files via upload

---
+++ GenericDelegates/Genericdelegate.cs
@@ -0,0 +1,36 @@
+﻿namespace GenericDelegates
+{
+    public class Genericdelegate
+    {
+        static void Main(string[] args)
+        {
+            //Action delegate
+            List<string> names = new List<string> { "Tejaswini", "Swathi", "Shalini" };
+            Action<List<string>> printNames = (list) => {
+                foreach (string word in list)
+                {
+                    Console.WriteLine(word);
+                }
+            };
+
+            printNames(names);
+
+            //Func delegate
+            Func<double, double, double> RectangleArea = (length, width) => length * width;
+            double area = RectangleArea(10.5, 20.5);
+            Console.WriteLine(area);
+
+            //Predicate delegate
+            Predicate<string> isUpper = IsUpperCase;
+
+            bool result = isUpper("hello world!!");
+
+            Console.WriteLine(result);
+
+            static bool IsUpperCase(string str)
+            {
+                return str.Equals(str.ToUpper());
+            }
+        }
+    }
+}
