8af98857266f929aae75128989857b67fec62be5
Tejaswini M <86282374+Tejaswini-052@users.noreply.github.com>
2023-04-14 19:54:53 +0530

    Add files via upload

---
+++ Interval/interval.cs
@@ -0,0 +1,39 @@
+﻿namespace Interval
+{
+    public class interval
+    {
+        static void Main(string[] args)
+        {
+            int[][] IntervalNumbers = new int[][] {
+            new int[] {1, 3},
+            new int[] {2, 6},
+            new int[] {8, 10},
+            new int[] {15, 19}
+        };
+            List<int[]> Combine = new List<int[]>();
+
+                foreach (int[] value in IntervalNumbers)
+                {
+                    int i = 0;
+                    while (i < Combine.Count && value[0] > Combine[i][1])
+                    {
+                        i++;
+                    }
+                    while (i < Combine.Count && value[1] >= Combine[i][0])
+                    {
+                         value[0] = Math.Min(value[0], Combine[i][0]);
+                         value[1] = Math.Max(value[1], Combine[i][1]);
+                    Combine.RemoveAt(i);
+                    }
+                
+                   Combine.Insert(i, value);
+                }
+
+            foreach (int[] item in Combine)
+            {
+                Console.WriteLine("[" + item[0] + ", " + item[1] + "]");
+            }
+        }
+
+        }
+}
