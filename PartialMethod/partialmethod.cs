bea7a2a2eeefadcafa1283e55f1e2551601436e4
Tejaswini M <86282374+Tejaswini-052@users.noreply.github.com>
2023-04-14 19:09:08 +0530

    Add files via upload

---
+++ PartialMethod/partialmethod.cs
@@ -0,0 +1,30 @@
+﻿namespace PartialMethod
+{
+    public class partialmethod
+    {
+        public partial class Prograd
+        {
+            partial void NumberOfstudents(int value);
+
+            public void CallNumberOfstudents()
+            {
+
+                NumberOfstudents(5);
+            }
+        }
+        public partial class Prograd
+        {
+
+            partial void NumberOfstudents(int value)
+            {
+                Console.WriteLine("Number of students in prograd are: " + value);
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            Prograd StudentObj = new Prograd();
+            StudentObj.CallNumberOfstudents();
+        }
+    }
+}
